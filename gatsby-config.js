979423ae84d0616b8472132b77d45daefdaf89e6
Gabe <jflores353@gmail.com>
2020-03-10 23:00:48 -0500

    Created Cancel Order and Success Order pages. Changed Main title in Config. Created placeOrder( ) to use sku from Stripe and redirect after Cancel or Success. Made select list with 3 sku's.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby Ecommerce Site`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Gabe Flores`,
   },
   plugins: [
     `gatsby-plugin-stripe`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,29 +8,32 @@ import SEO from "../components/seo"
 const IndexPage = () => {
   const stripe = window.Stripe("pk_test_l9j6PmKp06OAyBzSs2TLesVF00NxbCZESj")
 
-  const placeOrder = () => {
+  const placeOrder = sku => {
     stripe.redirectToCheckout({
       items: [
         {
-          sku: "sku_Gt6nrPKYYZddxQ",
+          sku,
           quantity: 1,
         },
       ],
-      successUrl: "http://localhost:8000/",
-      cancelUrl: "http://localhost:8000/",
+      successUrl: "http://localhost:8000/success",
+      cancelUrl: "http://localhost:8000/cancel",
     })
   }
 
   return (
     <Layout>
       <SEO title="Home" />
-      <h1>Hi people</h1>
-      <p>Welcome to your new Gatsby site.</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
+      <div>
+        <img src="https://picsum.photos/340/400" alt="Web Dev Shirt" />
+        <h3>Web Dev T-Shirt</h3>
+        <select>
+          <option value="sku_Gt7tetiDGBd2B2">Small</option>
+          <option value="sku_Gt7tPhEdeUU93w">Medium</option>
+          <option value="sku_Gt6nrPKYYZddxQ">Large</option>
+        </select>
+        <button onClick={() => placeOrder("sku_Gt6nrPKYYZddxQ")}>Buy Me</button>
       </div>
-      <Link to="/page-2/">Go to page 2</Link>
     </Layout>
   )
 }
