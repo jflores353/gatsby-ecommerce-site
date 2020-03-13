c2251c2148a5b1e37d6745bdbeed60f1aeb8f2a3
Gabe <jflores353@gmail.com>
2020-03-12 20:22:05 -0500

    In Products set skus to be set to product id and passed prop to product. In product set options to map skus and to return option with same info as before.

---
+++ src/components/product.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react"
+
+const Product = ({ skus, product }) => {
+  const stripe = window.Stripe("pk_test_l9j6PmKp06OAyBzSs2TLesVF00NxbCZESj")
+  const [sku, setSku] = useState(skus[0].node.id)
+
+  const placeOrder = () => {
+    stripe.redirectToCheckout({
+      items: [
+        {
+          sku,
+          quantity: 1,
+        },
+      ],
+      successUrl: "http://localhost:8000/success",
+      cancelUrl: "http://localhost:8000/cancel",
+    })
+  }
+
+  return (
+    <div>
+      <img src="https://picsum.photos/340/400" alt="Web Dev Shirt" />
+      <h3>{product.name}</h3>
+      <select value={sku} onChange={e => setSku(e.target.value)}>
+        {skus.map(edge => (
+          <option key={edge.node.id} value={edge.node.id}>
+            {edge.node.attributes.name}
+          </option>
+        ))}
+      </select>
+      <button onClick={placeOrder}>Buy Me</button>
+    </div>
+  )
+}
+export default Product
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,40 +1,14 @@
-import React, { useState } from "react"
-import { Link } from "gatsby"
+import React from "react"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+import Products from "../components/Products"
 import SEO from "../components/seo"
 
 const IndexPage = () => {
-  const stripe = window.Stripe("pk_test_l9j6PmKp06OAyBzSs2TLesVF00NxbCZESj")
-  const [sku, setSku] = useState("sku_Gt7tetiDGBd2B2")
-
-  const placeOrder = () => {
-    stripe.redirectToCheckout({
-      items: [
-        {
-          sku,
-          quantity: 1,
-        },
-      ],
-      successUrl: "http://localhost:8000/success",
-      cancelUrl: "http://localhost:8000/cancel",
-    })
-  }
-
   return (
     <Layout>
       <SEO title="Home" />
-      <div>
-        <img src="https://picsum.photos/340/400" alt="Web Dev Shirt" />
-        <h3>Web Dev T-Shirt</h3>
-        <select value={sku} onChange={e => setSku(e.target.value)}>
-          <option value="sku_Gt7tetiDGBd2B2">Small</option>
-          <option value="sku_Gt7tPhEdeUU93w">Medium</option>
-          <option value="sku_Gt6nrPKYYZddxQ">Large</option>
-        </select>
-        <button onClick={placeOrder}>Buy Me</button>
-      </div>
+      <Products />
     </Layout>
   )
 }
